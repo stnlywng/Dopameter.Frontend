8eb9ac13288c01cc254faf5a87b04863d3cf484c
Stanley Wang <s927wang@uwaterloo.ca>
2024-11-23 01:27:55 -0500

    remove demo and how it works when logged in

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -14,44 +14,48 @@ const TopBar = () => {
       <div className={styles.container}>
         <div className={styles.logo}>
           <b onClick={() => navigate("/")}>Dopameter</b>
-          <Button
-            borderColor={"white"}
-            color={"white"}
-            borderRadius={8}
-            borderWidth={1}
-            background={"transparent"}
-            onClick={() => navigate("/info")}
-            size={"sm"}
-            _hover={{
-              backgroundColor: "#fafafa",
-              color: "black",
-              borderColor: "black",
-            }}
-            width={150}
-            height={9}
-            marginRight={"10px"}
-            marginLeft={"20px"}
-          >
-            How it works.
-          </Button>
-          <Button
-            borderColor={"white"}
-            color={"white"}
-            borderRadius={8}
-            borderWidth={1}
-            background={"transparent"}
-            size={"sm"}
-            _hover={{
-              backgroundColor: "#fafafa",
-              color: "black",
-              borderColor: "black",
-            }}
-            width={150}
-            height={9}
-            onClick={() => navigate("/demo")}
-          >
-            See a Demo.
-          </Button>
+          {!isAuth && (
+            <>
+              <Button
+                borderColor={"white"}
+                color={"white"}
+                borderRadius={8}
+                borderWidth={1}
+                background={"transparent"}
+                onClick={() => navigate("/info")}
+                size={"sm"}
+                _hover={{
+                  backgroundColor: "#fafafa",
+                  color: "black",
+                  borderColor: "black",
+                }}
+                width={150}
+                height={9}
+                marginRight={"10px"}
+                marginLeft={"20px"}
+              >
+                How it works.
+              </Button>
+              <Button
+                borderColor={"white"}
+                color={"white"}
+                borderRadius={8}
+                borderWidth={1}
+                background={"transparent"}
+                size={"sm"}
+                _hover={{
+                  backgroundColor: "#fafafa",
+                  color: "black",
+                  borderColor: "black",
+                }}
+                width={150}
+                height={9}
+                onClick={() => navigate("/demo")}
+              >
+                See a Demo.
+              </Button>
+            </>
+          )}
         </div>
 
         <div className={styles.buttons}>
